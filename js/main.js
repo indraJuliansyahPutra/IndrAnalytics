66d0d19b798308e1d32bf28a774dc0727cb6d384
indraJuliansyahPutra <mrindrajuliansyahputra10@gmail.com>
2025-06-25 11:29:39 +0700

    Add back-to-top button, refresh-to-top, and section-based URL

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 import { initHamburgerMenu } from './hamburgerMenu.js';
+import { initRefresh } from './refresh.js';
 import { initTypewriter } from './typeWriter.js';
 import { initLoadMoreProjects } from './loadMoreProjects.js';
 import { slideCertificate } from './slideCertificate.js';
 import { initModalsProject } from './modalsProject.js';
+import { removeSections } from './sections.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initHamburgerMenu();
+  initRefresh();
   initTypewriter();
   initLoadMoreProjects();
   slideCertificate();
   initModalsProject();
+  removeSections();
 });
